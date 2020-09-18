225b9eb5a3a6557b4282417851b2d5daa16c8ce5
satvick <satvickacharya16@gmail.com>
2020-09-18 08:38:18 -0400

    edited java program

--- java_programs/src/tictactoe/Main.java
+++ java_programs/src/tictactoe/Main.java
@@ -41,7 +41,23 @@ public class Main {
 		System.out.println("CPU move:\n\n");
 		
 		ArrayList<Integer> newOptions = new ArrayList<Integer>();
-		for (int i: options) {
+		for (int option: options) {
+			if (!(board[option] == "X" || board[option] == "O")) {
+				newOptions.add(option);
+			}
+		}
+		
+		Random rand2 = new Random();
+		int choice2 = rand2.nextInt(newOptions.size());
+		board[choice2] = "X";
+		showBoard(board);
+		
+		while (true) {
+			pMove = getPMove(board);
+			board[pMove] = "O";
+			System.out.println("");
+			showBoard(board);
+			
 			
 		}
 	}
@@ -70,6 +86,40 @@ public class Main {
 		int move = getPMove.nextInt();
 		
 		return move - 1;
+	}
+	
+	public static void checkForWin(String[] board) {
+		if (board[0] == board[1] == board[2] == "X") {
+	        System.out.println("CPU WINS!");
+	        exit()
+		} elif board[3] == board[4] == board[5] == "X":
+	        print("CPU WINS!")
+	        time.sleep(DELAY)
+	        exit()
+	    elif board[6] == board[7] == board[8] == "X":
+	        print("CPU WINS!")
+	        time.sleep(DELAY)
+	        exit()
+	    elif board[0] == board[3] == board[6] == "X":
+	        print("CPU WINS!")
+	        time.sleep(DELAY)
+	        exit()
+	    elif board[1] == board[4] == board[7] == "X":
+	        print("CPU WINS!")
+	        time.sleep(DELAY)
+	        exit()
+	    elif board[2] == board[5] == board[8] == "X":
+	        print("CPU WINS!")
+	        time.sleep(DELAY)
+	        exit()
+	    elif board[0] == board[4] == board[8] == "X":
+	        print("CPU WINS!")
+	        time.sleep(DELAY)
+	        exit()
+	    elif board[2] == board[4] == board[6] == "X":
+	        print("CPU WINS!")
+	        time.sleep(DELAY)
+	        exit()
 	}
 
 }
